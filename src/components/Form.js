54bebf34cdf9351c89733527e9bed8f1054344a1
abraham <apolichenco@gmail.com>
2022-11-27 22:39:07 -0500

    working on form dropdowns

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,66 +2,76 @@ import React, {useState} from "react";
 
 
 function Form({handleNew, allData}) {
-  // const [newCategory, setNewCategory] = useState()
+  const [dropdownCategory, setDropdownCategory] = useState()
   const [newProduct, setNewProduct] = useState()
   const [newPrice, setNewPrice] = useState()
 
-  // function handleNewCategory(event) {
-  //   setNewCategory(event.target.value)
-  // }
 
   function handleNewProduct(event) {
     setNewProduct(event.target.value)
+    console.log(event.target.value)
   }
 
   function handleNewPrice(event) {
     setNewPrice(event.target.value)
+    console.log(event.target.value)
   }
 
    function handleSubmitProduct(e) {
     e.preventDefault()
-    fetch("http://localhost:9292/products", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        privateEvent: newProduct
-      })
-    })
-    .then((r) => r.json())
-    .then((data) => handleNew(data))
+    console.log(newProduct)
+    // const newProduct = {
+    //   name: newProduct,
+    //   category_id:
+    // }
+    // fetch("http://localhost:9292/products", {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newProduct)
+    // })
+    // .then((r) => r.json())
+    // .then((data) => handleNew(data))
    }
 
    function handleSubmitPrice(e) {
     e.preventDefault()
-    fetch("http://localhost:9292/prices", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        privateEvent: newPrice
-      })
-    })
-    .then((r) => r.json())
-    .then((data) => handleNew(data))
+    console.log(newPrice)
+    // const newPrice = {
+    //   price: newPrice,
+    //   product_id: 
+    // }
+    // fetch("http://localhost:9292/prices", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newPrice)
+    // })
+    // .then((r) => r.json())
+    // .then((data) => handleNew(data))
    }
 
   return (
     <div>
-      {/* <form onSubmit={handleSubmitCategory}>
-      <label>New Category</label><br></br>
-      <input type="text" id="new-category" name="new-category" onChange={handleNewCategory}></input>     
-    </form> */}
-    <form onSubmit={handleSubmitProduct}>
-    <label>New product</label><br></br>
-      <input type="text" id="new-product" name="new-product" onChange={handleNewProduct}></input>
-    </form>
-    <form onSubmit={handleSubmitPrice}>
-    <label>New price</label><br></br>
-      <input type="text" id="new-price" name="new-price" onChange={handleNewPrice}></input> 
-    </form>
+      <form onSubmit={handleSubmitProduct}>
+        <select>
+          {allData.map((category) => <option key={category.id} value={category.id}>{category.name}</option>)}
+        </select><br></br>
+        <label>New product</label><br></br>
+          <input type="text" id="new-product" name="new-product" onChange={handleNewProduct}></input>
+      </form>
+      <form onSubmit={handleSubmitPrice}>
+      <select>
+          {allData.map((category) => <option key={category.id} value={category.id} onChange={(value) => setDropdownCategory(value)}>{category.name}</option>)}
+        </select><br></br>
+        {/* <select>
+          {allData.map((category) => <option key={category.id} value={category.id}>{category.name}</option>)}
+        </select> */}
+        <label>New price</label><br></br>
+          <input type="text" id="new-price" name="new-price" onChange={handleNewPrice}></input> 
+      </form>
     </div>
   );
 }
